9f75de6470a039132afbe3c7d6f528475bdf09a7
cassiusbessa <cassiusbessa@gmail.com>
2022-08-18 19:16:07 -0300

    requisito 1 concluído

---
+++ app/backend/src/database/models/matches.ts
@@ -0,0 +1,48 @@
+import { INTEGER, Model } from 'sequelize';
+import db from '.';
+
+class Matches extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: number;
+}
+Matches.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'home_team',
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'home_team_goals',
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'away_team',
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'away_team_goals',
+  },
+  inProgress: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'in_progress',
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
---
+++ app/backend/src/database/models/teams.ts
@@ -0,0 +1,24 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  id!: number;
+  teamName!: string;
+}
+Teams.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
